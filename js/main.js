5e932d2eed08d4abd6fd32754751e845d28c0242
TateMbuva <tatembuva@hotmail.com>
2017-11-03 13:29:01 +0200

    assigment

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+
+	var typed2 = new Typed('#typed2', {
+    strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: false,
+    loop: true
+  });
+ 	
+ });
+ 
